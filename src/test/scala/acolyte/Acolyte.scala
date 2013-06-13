f27526ceb8b6649467316f88177c9839d1618854
cchantep <c.chantepie@yahoo.fr>
2013-06-13 23:51:56 +0200

    Update DSL

--- src/test/scala/acolyte/Acolyte.scala
+++ src/test/scala/acolyte/Acolyte.scala
@@ -41,14 +41,12 @@ final class ScalaRuleStatementHandler(
 
 final class ScalaResultRow(r: Row) extends Row {
   lazy val cells = r.cells
-  def cell(n: String) = r.cell(n)
 
-  lazy val list: List[Any] = 
+  lazy val list: List[Any] =
     JavaConversions.iterableAsScalaIterable(cells).foldLeft(List[Any]()) {
-      (l, v) => l :+ v
+      (l, v) ⇒ l :+ v
     }
 
-  def opt(n: String): Option[Any] = Option(cell(n)).map(_.value)
 }
 
 final class ScalaRowList[R <: Row](l: RowList[R]) extends RowList[R] {
@@ -57,6 +55,9 @@ final class ScalaRowList[R <: Row](l: RowList[R]) extends RowList[R] {
 
   // Extension
   def :+(row: R) = append(row)
+
+  def withLabels(labels: (Int, String)*): RowList[R] =
+    labels.foldLeft[RowList[R]](this) { (l, t) ⇒ l.withLabel(t._1, t._2) }
 }
 
 // Acolyte DSL
@@ -65,7 +66,7 @@ object Acolyte {
 
   implicit def RuleStatementHandlerAsScala(h: RuleStatementHandler): ScalaRuleStatementHandler = new ScalaRuleStatementHandler(h)
 
-  implicit def ResultRowAsScala[R <: Row](r: R): ScalaResultRow = 
+  implicit def ResultRowAsScala[R <: Row](r: R): ScalaResultRow =
     new ScalaResultRow(r)
 
   implicit def RowListAsScala[R <: Row](l: RowList[R]): ScalaRowList[R] =
@@ -74,14 +75,8 @@ object Acolyte {
   def rowList[R <: Row]: RowList[R] = new RowList[R]
 
   def row1[A](c1: A): Row.Row1[A] = RowList.row1(c1)
-  def row1[A](c1: A, n1: String): Row.Row1[A] = RowList.row1(c1, n1)
-
   def row2[A, B](c1: A, c2: B): Row.Row2[A, B] = RowList.row2(c1, c2)
-  def row2[A, B](c1: A, n1: String, c2: B, n2: String): Row.Row2[A, B] =
-    RowList.row2(c1, n1, c2, n2)
-
   def row3[A, B, C](c1: A, c2: B, c3: C): Row.Row3[A, B, C] =
     RowList.row3(c1, c2, c3)
 
-  def row3[A, B, C](c1: A, n1: String, c2: B, n2: String, c3: C, n3: String): Row.Row3[A, B, C] = RowList.row3(c1, n1, c2, n2, c3, n3)
 }
