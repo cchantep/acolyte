2ae96def6ee5f0555138d81e162c94970dcc224c
cchantep <chantepie@altern.org>
2015-07-18 22:53:01 +0200

    Update Java & Scala version

--- project/Acolyte.scala
+++ project/Acolyte.scala
@@ -10,15 +10,21 @@ object Acolyte extends Build with Dependencies
 
   val versionVariant = if (isJavaAtLeast("1.7")) "-j7p" else ""
 
+  val javaVersion =
+    if (isJavaAtLeast("1.8")) "1.8"
+    else if (isJavaAtLeast("1.7")) "1.7"
+    else "1.6"
+
   lazy val root = Project(id = "acolyte", base = file(".")).
     aggregate(scalacPlugin, reactiveMongo,
       jdbcDriver, jdbcScala, jdbcClojure, studio).
     settings(
       organization in ThisBuild := "org.eu.acolyte",
       version in ThisBuild := s"1.0.33${versionVariant}",
-      //javaOptions in ThisBuild ++= Seq("-source", "1.6", "-target", "1.6"),
+      javaOptions in ThisBuild ++= Seq(
+        "-source", javaVersion, "-target", javaVersion),
       scalaVersion in ThisBuild := "2.10.4",
-      crossScalaVersions in ThisBuild := Seq("2.10.4", "2.11.6"),
+      crossScalaVersions in ThisBuild := Seq("2.10.4", "2.11.7"),
       publishTo in ThisBuild := Some(Resolver.file("file", 
         new File(Path.userHome.absolutePath+"/.m2/repository"))),
       homepage := Some(url("http://acolyte.eu.org")),
