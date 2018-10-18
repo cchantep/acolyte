9b27b1361a2aca902421a70774e4765e02804bb2
TANKOUA Stéphane <s.tankouat@gmail.com>
2018-10-18 20:48:46 +0200

    Add resource handler parameter for AcolyteDatabase (#91)
    * Add resource handler parameter for AcolyteDatabase
    * Adding test for play jdbc transaction handler

--- play-jdbc/src/test/scala/PlayJdbcSpec.scala
+++ play-jdbc/src/test/scala/PlayJdbcSpec.scala
@@ -1,9 +1,7 @@
 package acolyte.jdbc.play
 
-import play.api.db.Database
 import anorm._
-
-import acolyte.jdbc.Implicits._
+import play.api.db.Database
 
 class PlaySpec extends org.specs2.mutable.Specification {
   "Acolyte/Play JDBC" title
@@ -11,11 +9,47 @@ class PlaySpec extends org.specs2.mutable.Specification {
   sequential
 
   "Anorm use case #1" should {
-    val runner1 = PlayJdbcDSL.withPlayDBResult("foo")
+    val runner1 = PlayJdbcUseCases.useCase1
 
     "return a DB related string" in runner1 { db: Database ⇒
       db.withConnection { implicit con ⇒
         SQL"SELECT test".as(SqlParser.scalar[String].single) must_== "foo"
+      }
+    }
+  }
+
+  "Anorm use case #2" should {
+    @volatile var count = 0
+    val runner2 = PlayJdbcUseCases.useCase2 { count = count + 1 }
+
+    "should increment count on commit" in runner2 { db: Database ⇒
+      db.withConnection { implicit con ⇒
+        con.setAutoCommit(false)
+        SQL"insert into foo values (1, 'foo value')".executeInsert() must beSome(1) and {
+          SQL"insert into bar values (1, 'bar value')".executeInsert() must beSome(1)
+        } and {
+          con.commit() must not(throwA[Exception])
+        } and {
+          count must_=== 1
+        }
+      }
+    }
+  }
+
+  "Anorm use case #3" should {
+    @volatile var count = 1
+    val runner3 = PlayJdbcUseCases.useCase3 { count = count - 1 }
+
+    "should decrement count on rollback" in runner3 { db: Database ⇒
+      db.withConnection { implicit con ⇒
+        con.setAutoCommit(false)
+        SQL"insert into foo values (1, 'foo value')".executeInsert() must beSome(1) and {
+          SQL"insert into bar values (1, 'bar value')".executeInsert() must throwA[java.sql.SQLException]("Simulating on error.")
+        } and {
+          con.rollback() must not(throwA[Exception])
+        } and {
+          count must_=== 0
+        }
       }
     }
   }
