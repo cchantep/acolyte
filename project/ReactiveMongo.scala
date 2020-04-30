b959390c5419621b6d44cabeae2d384fe18e8319
Cédric Chantepie <cchantep@users.noreply.github.com>
2020-04-30 23:12:41 +0200

    Update ReactiveMongo.scala

--- project/ReactiveMongo.scala
+++ project/ReactiveMongo.scala
@@ -12,7 +12,7 @@ class ReactiveMongo(scalacPlugin: Project) { self =>
   lazy val generatedClassDirectory = settingKey[File](
     "Directory where classes get generated")
 
-  val reactiveMongoVer = "0.20.9"
+  val reactiveMongoVer = "0.20.10"
 
   lazy val project =
     Project(id = "reactive-mongo", base = file("reactive-mongo")).
