d5190d7d4f2816c5285c4e6c86f477a5af9f7c70
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-02-06 22:49:47 +0100

    Update slf4j-simple to 2.0.12 (#345)

--- project/ReactiveMongo.scala
+++ project/ReactiveMongo.scala
@@ -27,7 +27,7 @@ final class ReactiveMongo { self =>
       },
       libraryDependencies ++= Seq(
         "org.reactivemongo" %% "reactivemongo" % reactiveMongoVer % Provided,
-        "org.slf4j" % "slf4j-simple" % "2.0.11" % Provided,
+        "org.slf4j" % "slf4j-simple" % "2.0.12" % Provided,
         "org.specs2" %% "specs2-core" % specsVer.value % Test
       ),
       libraryDependencies += {
