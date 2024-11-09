018cef11fb90261fda00e71888d56cd35adfd18b
Scala Steward <scala_steward@virtuslab.com>
2024-11-09 14:52:34 +0000

    Update anorm to 2.8.0

--- project/PlayJdbc.scala
+++ project/PlayJdbc.scala
@@ -30,7 +30,7 @@ class PlayJdbc(jdbcScala: Project) {
         },
         // make sure plugin is there
         libraryDependencies ++= {
-          val anorm = "org.playframework.anorm" %% "anorm" % "2.7.0"
+          val anorm = "org.playframework.anorm" %% "anorm" % "2.8.0"
 
           Seq(
             "org.eu.acolyte" % "jdbc-driver" % (ThisBuild / version).value,
